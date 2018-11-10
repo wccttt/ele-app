766af236853c1b99c557dff9a6a1e4506965e095
wccttt <1843806686@qq.com>
2018-11-10 11:31:47 +0800

    add

--- src/component/SwipeList.js
+++ src/component/SwipeList.js
@@ -1,41 +1,41 @@
-import React,{Component} from 'react';
-import {render} from 'react-dom';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import ReactSwipe from 'react-swipe';
-import {NavLink} from 'react-router-dom'
-import  './SwipeList.less';
+import { NavLink } from 'react-router-dom'
+import './SwipeList.less';
 
-class SwipeList extends Component{
-    constructor(props){
+class SwipeList extends Component {
+    constructor(props) {
         super(props)
     }
-    render(){
-        let {data}=this.props;
-        return(
-<ReactSwipe>
-    <div className='swipeList'>
-        <ul className='clearfix'>
-            {
-data.map((item,index)=>{
-    return <li  className='listBox' key={index}>
-        <div className='container'>
-            <img src={item.link} alt={item.title}/>
-        </div>
-        <span className='title'>{item.title}</span>
+    render() {
+        let { data } = this.props;
+        return (
+            <ReactSwipe>
+                <div className='swipeList'>
+                    <ul className='clearfix'>
+                        {
+                            data.map((item, index) => {
+                                return <li className='listBox' key={index}>
+                                    <div className='container'>
+                                        <img src={item.link} alt={item.title} />
+                                    </div>
+                                    <span className='title'>{item.title}</span>
 
-    </li>
-})
-            }
-
-
-        </ul>
+                                </li>
+                            })
+                        }
 
 
-    </div>
-</ReactSwipe>
+                    </ul>
+
+
+                </div>
+            </ReactSwipe>
 
         )
     }
 }
-export default  connect()(SwipeList)
+export default connect()(SwipeList)
